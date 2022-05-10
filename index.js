54d2c3a77e22ab3b271ef96d7ebb6a147d878e10
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-05-10 04:37:16 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// create a function that reverses a string:
+// 'Hi, My Name is Jason' should be:
+// 'nosaJ si emaN yM ,iH'
+
+function reverse(str){
+  // check input
+  if (!str || str.length < 2) {
+    return "Thats not a string"
+  }
+  
+}
